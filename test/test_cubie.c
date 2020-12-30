62a94d2596d3734c54c8f73da539d4042575f73a
h3nnn4n <uber.renan@gmail.com>
2020-12-29 23:37:22 -0300

    Test corner multiply

--- test/test_cubie.c
+++ test/test_cubie.c
@@ -78,6 +78,40 @@ void test_multiply_cube_cubie_edges_u4_is_identity() {
     free(cube);
 }
 
+void test_multiply_cube_cubie_corners_u2_is_not_identity() {
+    cube_cubie *cube         = init_cubie_cube();
+    cube_cubie *cube_u1_move = build_basic_move(MOVE_U1);
+
+    multiply_cube_cubie_corners(cube, cube_u1_move);
+    multiply_cube_cubie_corners(cube, cube_u1_move);
+
+    int off_count = 0;
+    for (int i = 0; i < 8; i++) {
+        if (i != (int)cube->corner_permutations[i])
+            off_count++;
+    }
+
+    TEST_ASSERT_EQUAL_INT(4, off_count);
+
+    free(cube);
+}
+
+void test_multiply_cube_cubie_corners_u4_is_identity() {
+    cube_cubie *cube         = init_cubie_cube();
+    cube_cubie *cube_u1_move = build_basic_move(MOVE_U1);
+
+    for (int i = 0; i < 4; i++) {
+        multiply_cube_cubie_corners(cube, cube_u1_move);
+    }
+
+    for (int i = 0; i < 8; i++) {
+        TEST_ASSERT_EQUAL_INT(i, cube->corner_permutations[i]);
+        TEST_ASSERT_EQUAL_INT(0, cube->corner_orientations[i]);
+    }
+
+    free(cube);
+}
+
 void setUp(void) {}
 
 void tearDown(void) {}
@@ -88,6 +122,11 @@ int main() {
     RUN_TEST(test_init_cubie_edge_permutations);
     RUN_TEST(test_init_cubie_corner_orientations);
     RUN_TEST(test_init_cubie_edge_orientations);
+
+    RUN_TEST(test_multiply_cube_cubie_edges_u2_is_not_identity);
     RUN_TEST(test_multiply_cube_cubie_edges_u4_is_identity);
+
+    RUN_TEST(test_multiply_cube_cubie_corners_u2_is_not_identity);
+    RUN_TEST(test_multiply_cube_cubie_corners_u4_is_identity);
     return UNITY_END();
 }
