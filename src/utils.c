d51ab3d1110643509e9062bf9f298bdba247be73
h3nnn4n <uber.renan@gmail.com>
2020-12-30 21:45:12 -0300

    Helper for converting move enums to strings

--- src/utils.c
+++ src/utils.c
@@ -1,4 +1,11 @@
+#include <assert.h>
+
 #include "cubie.h"
+
+static char *_move_t_to_str[] = {
+    "MOVE_U2", "MOVE_U2", "MOVE_U3", "MOVE_R1", "MOVE_R2", "MOVE_R3", "MOVE_F1", "MOVE_F2", "MOVE_F3",   "MOVE_D1",
+    "MOVE_D2", "MOVE_D3", "MOVE_L1", "MOVE_L2", "MOVE_L3", "MOVE_B1", "MOVE_B2", "MOVE_B3", "MOVE_NULL",
+};
 
 int cubie_off_count(cube_cubie *cube) {
     int count = 0;
@@ -23,3 +30,10 @@ int cubie_off_count(cube_cubie *cube) {
 }
 
 int is_solved(cube_cubie *cube) { return cubie_off_count(cube) == 0; }
+
+char *move_to_str(move_t move) {
+    assert(move >= 0);
+    assert(move <= 18);
+
+    return _move_t_to_str[move];
+}
--- src/utils.h
+++ src/utils.h
@@ -5,5 +5,6 @@
 
 int is_solved(cube_cubie *cube);
 int cubie_off_count(cube_cubie *cube);
+char *move_to_str(move_t move);
 
 #endif /* end of include guard */
