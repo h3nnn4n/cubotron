b9084bf7c275979bc3233ab8f53388ed5c73b106
h3nnn4n <uber.renan@gmail.com>
2020-12-31 18:10:38 -0300

    Add brute force sanity testing for coord cubes

--- test/test_coord_move_tables.c
+++ test/test_coord_move_tables.c
@@ -1,3 +1,4 @@
+#include <pcg_variants.h>
 #include <unity.h>
 
 #include <coord_cube.h>
@@ -5,6 +6,8 @@
 #include <cubie_move_table.h>
 #include <definitions.h>
 #include <utils.h>
+
+static pcg32_random_t rng;
 
 void test_coord_sanity_edge_orientations() {
     coord_cube_t *coord_cube = get_coord_cube();
@@ -46,15 +49,37 @@ void test_coord_sanity_corner_orientations() {
     free(coord_cube);
 }
 
+void test_coord_sanity_brute_force() {
+    coord_cube_t *coord_cube = get_coord_cube();
+    cube_cubie_t *cubie_cube = init_cubie_cube();
+
+    for (int i = 0; i < 10000; i++) {
+        move_t move = pcg32_boundedrand_r(&rng, N_MOVES);
+
+        cubie_apply_move(cubie_cube, move);
+        coord_apply_move(coord_cube, move);
+
+        TEST_ASSERT_EQUAL_INT(get_corner_orientations(cubie_cube), coord_cube->corner_orientations);
+        TEST_ASSERT_EQUAL_INT(get_edge_orientations(cubie_cube), coord_cube->edge_orientations);
+    }
+
+    free(cubie_cube);
+    free(coord_cube);
+}
+
 void setUp(void) { build_move_tables(); }
 
 void tearDown(void) {}
 
 int main() {
+    pcg32_srandom_r(&rng, 42u, 54u);
+
     UNITY_BEGIN();
 
     RUN_TEST(test_coord_sanity_edge_orientations);
     RUN_TEST(test_coord_sanity_corner_orientations);
+
+    RUN_TEST(test_coord_sanity_brute_force);
 
     return UNITY_END();
 }
