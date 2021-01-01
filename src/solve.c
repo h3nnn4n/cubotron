2405d8b8a1dcfab7b7322615c19e4b06c2d84f38
h3nnn4n <uber.renan@gmail.com>
2021-01-01 15:05:40 -0300

    Move solver routine to separate file

---
+++ src/solve.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "coord_move_tables.h"
+#include "cubie_cube.h"
+#include "utils.h"
+
+#define max_moves 4
+
+move_t *solve(coord_cube_t *cube) {
+    move_t *solution = malloc(sizeof(move_t) * 30); // HACK
+
+    move_t        move_stack[max_moves];
+    coord_cube_t *cube_stack[max_moves];
+    int           pivot      = 0;
+    int           move_count = 0;
+
+    char buffer[512];
+
+    for (int i = 0; i < max_moves; i++) {
+        move_stack[i] = -1;
+        cube_stack[i] = random_coord_cube();
+    }
+
+    copy_coord_cube(cube_stack[0], cube);
+    printf("scrambled cube: %d %d\n\n", cube->edge_orientations, cube->corner_orientations);
+
+    /*sprintf(buffer, " moves: %4d pivot: %2d", move_count, pivot);*/
+    /*sprintf(buffer, "%s : %7d %7d", buffer, cube->edge_orientations, cube->corner_orientations);*/
+    /*printf("%s\n", buffer);*/
+
+    do {
+        move_stack[pivot]++;
+
+        if (move_stack[pivot] >= N_MOVES) {
+            move_stack[pivot] = -1;
+            /*printf("\n");*/
+            pivot--;
+
+            if (pivot < 0) {
+                printf("failed to solve\n");
+                break;
+            } else if (pivot == 0) {
+                copy_coord_cube(cube_stack[0], cube);
+            } else if (pivot > 0) {
+                copy_coord_cube(cube_stack[pivot], cube_stack[pivot - 1]);
+            }
+
+            continue;
+        }
+
+        coord_apply_move(cube_stack[pivot], move_stack[pivot]);
+        move_count++;
+
+        if (move_count % 10000000 == 0) {
+            sprintf(buffer, " moves: %4d pivot: %2d", move_count, pivot);
+            sprintf(buffer, "%s : %7d %7d -> ", buffer, cube_stack[pivot]->edge_orientations,
+                    cube_stack[pivot]->corner_orientations);
+            for (int i = 0; i <= pivot; i++)
+                sprintf(buffer, "%s %s", buffer, move_to_str(move_stack[i]));
+            printf("%s\n", buffer);
+        }
+
+        if (cube_stack[pivot]->edge_orientations + cube_stack[pivot]->corner_orientations == 0) {
+            buffer[0] = '\0';
+            for (int i = 0; i <= pivot; i++)
+                sprintf(buffer, "%s %s", buffer, move_to_str(move_stack[i]));
+            printf("%s\n", buffer);
+
+            break;
+        }
+
+        if (pivot < max_moves - 1) {
+            copy_coord_cube(cube_stack[pivot + 1], cube_stack[pivot]);
+            pivot++;
+        } else {
+            copy_coord_cube(cube_stack[pivot], cube_stack[pivot - 1]);
+        }
+    } while (1);
+
+    return solution;
+}
---
+++ src/solve.h
@@ -0,0 +1,8 @@
+#ifndef _SOLVE
+#define _SOLVE
+
+#include "coord_cube.h"
+
+move_t *solve(coord_cube_t *cube);
+
+#endif /* end of include guard */
