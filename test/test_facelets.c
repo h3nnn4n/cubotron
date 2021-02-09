ee2184654731ca2dcdf25d7ea15a87e5f6b4b022
h3nnn4n <uber.renan@gmail.com>
2021-02-09 09:16:16 -0300

    Test cubie cube from facelets using scrambled cube

--- test/test_facelets.c
+++ test/test_facelets.c
@@ -35,6 +35,29 @@ void test_build_cube_from_facelet_string_solved() {
     TEST_ASSERT_EQUAL_INT(0, corner_permutations);
 }
 
+void test_build_cube_from_facelet_string_scrambled() {
+    char          facelets[N_FACELETS] = "BBURUDBFUFFFRRFUUFLULUFUDLRRDBBDBDBLUDDFLLRRBRLLLBRDDF";
+    cube_cubie_t *cubie_cube           = build_cubie_cube_from_str(facelets);
+
+    int edge_orientation = get_edge_orientations(cubie_cube);
+    TEST_ASSERT_EQUAL_INT(841, edge_orientation);
+
+    int E_slice = get_E_slice(cubie_cube);
+    TEST_ASSERT_EQUAL_INT(80, E_slice);
+
+    int sorted_E_slice = get_E_sorted_slice(cubie_cube);
+    TEST_ASSERT_EQUAL_INT(1932, sorted_E_slice);
+
+    int UD6_edges = get_UD6_edges(cubie_cube);
+    TEST_ASSERT_EQUAL_INT(101699, UD6_edges);
+
+    int corner_orientations = get_corner_orientations(cubie_cube);
+    TEST_ASSERT_EQUAL_INT(591, corner_orientations);
+
+    int corner_permutations = get_corner_permutations(cubie_cube);
+    TEST_ASSERT_EQUAL_INT(33724, corner_permutations);
+}
+
 void setUp(void) {}
 
 void tearDown(void) {}
@@ -44,6 +67,7 @@ int main() {
 
     RUN_TEST(test_facelets_color_count);
     RUN_TEST(test_build_cube_from_facelet_string_solved);
+    RUN_TEST(test_build_cube_from_facelet_string_scrambled);
 
     return UNITY_END();
 }
