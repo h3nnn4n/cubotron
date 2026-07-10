1b63a887683881cf8d4e49fb2091ff10a4f5014d
h3nnn4n <git@renan.digital>
2026-07-10 11:10:57 -0300

    style: apply clang-format

--- src/solve.c
+++ src/solve.c
@@ -103,15 +103,15 @@ solve_list_t *solve_single(const coord_cube_t *original_cube) {
 
 solve_list_t *solve(const coord_cube_t *original_cube, const config_t *config) {
     if (is_coord_solved(original_cube)) {
-        solve_list_t *solves = new_solve_list_node();
-        solves->solution = malloc(sizeof(move_t));
-        solves->solution[0] = MOVE_NULL;
-        solves->phase1_solution = malloc(sizeof(move_t));
+        solve_list_t *solves       = new_solve_list_node();
+        solves->solution           = malloc(sizeof(move_t));
+        solves->solution[0]        = MOVE_NULL;
+        solves->phase1_solution    = malloc(sizeof(move_t));
         solves->phase1_solution[0] = MOVE_NULL;
-        solves->phase2_solution = malloc(sizeof(move_t));
+        solves->phase2_solution    = malloc(sizeof(move_t));
         solves->phase2_solution[0] = MOVE_NULL;
 
-        solve_stats_t *stats = get_solve_stats();
+        solve_stats_t *stats     = get_solve_stats();
         stats->phase1_depth      = 0;
         stats->phase2_depth      = 0;
         stats->solution_length   = 0;
@@ -121,7 +121,7 @@ solve_list_t *solve(const coord_cube_t *original_cube, const config_t *config) {
         stats->phase1_move_count = 0;
         stats->phase2_move_count = 0;
         stats->move_count        = 0;
-        solves->stats = stats;
+        solves->stats            = stats;
 
         return solves;
     }
