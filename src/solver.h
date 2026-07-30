8b5d1886432cddcce61e1314035275c84b281f6d
h3nnn4n <git@renan.digital>
2026-07-29 22:15:44 -0300

    chore: update copyright to 2026

--- src/solver.h
+++ src/solver.h
@@ -1,5 +1,5 @@
 /*
- * Copyright <2021> <Renan S Silva, aka h3nnn4n>
+ * Copyright <2026> <Renan S Silva, aka h3nnn4n>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
