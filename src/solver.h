e20ec347e6fb162881669022f40c87e744c73862
Renan S Silva <git@renan.digital>
2026-07-29 22:18:27 -0300

    feat: add puzzle and solver interface headers (#55)
    * feat: add puzzle and solver interface headers
    
    * style: apply clang-format to puzzle.h and solver.h
    
    * chore: update copyright to 2026

---
+++ src/solver.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright <2026> <Renan S Silva, aka h3nnn4n>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#ifndef _SOLVER_H
+#define _SOLVER_H
+
+#include "config.h"
+#include "puzzle.h"
+#include "solution.h"
+
+typedef struct solver_ops_s solver_ops_t;
+
+struct solver_ops_s {
+    const char *name;
+    const char *puzzle_name;
+
+    void (*init)(void);
+    solve_list_t *(*solve)(const puzzle_t *puzzle, const config_t *cfg);
+    void (*cleanup)(void);
+};
+
+void                solver_register(const solver_ops_t *ops);
+const solver_ops_t *solver_lookup(const char *puzzle_name);
+solve_list_t       *solve_puzzle(const char *puzzle_name, const char *state_str, const config_t *cfg);
+
+#endif
